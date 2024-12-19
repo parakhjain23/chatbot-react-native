d3d619d186f7ef333a89d67e8d0b017af71d167e
parakhjain23 <parakhjain2301@gmail.com>
2024-12-19 12:01:09 +0530

    install chatbot

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,22 @@
-import Chatbot from '@/components/Chatbot'
-import React, { useState } from 'react'
-import { Button, Text, View } from 'react-native'
+import ChatBot from 'chatbot-react-native-sdk';
+import React from 'react';
+import { Button, DeviceEventEmitter, Text, View } from 'react-native';
 
 function Home() {
-  const [isWebViewVisible, setIsWebViewVisible] = useState(false);
+  const [threadId, setThreadId] = React.useState('12345');
+  const changeThreadId = () => {
+    DeviceEventEmitter.emit('openChatbot', { type: 'openChatbot' });
+    // const randomThreadId = Math.random().toString(36).substring(2, 10);
+    // setThreadId(randomThreadId);
+  }
   return (
-    <>
-      <View style={{ position: 'absolute', bottom: 40, right: 20 }}>
-        <Button title='Chatbot' onPress={() => setIsWebViewVisible(!isWebViewVisible)} />
+    <View style={{ flex: 1 }}>
+      {/* <Button title="Change Thread ID" onPress={changeThreadId} /> */}
+      <View style={{ width: 200, height: 600, justifyContent: 'center', alignItems: 'center', backgroundColor: 'yellow', position: 'static'}}>
+        <ChatBot threadId={threadId} bridgeName='demo' embedToken="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcmdfaWQiOiIxMjg5IiwiY2hhdGJvdF9pZCI6IjY2NTk2YWU3ZjA0NGRlNzMzZTNlYzdlYiIsInVzZXJfaWQiOiIxMjM0In0.WPAYYTuTFoXaLJLrA_SR14eym4lxjtiJpamyk2GBOTE" openInContainer={false} />
       </View>
-      <Chatbot isWebViewVisible={isWebViewVisible} setIsWebViewVisible={setIsWebViewVisible}/>
-    </>
+
+    </View>
   )
 }
 
