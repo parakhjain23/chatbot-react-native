767e1b28e7b919fe7a6cd8952feb7b41856bf62c
parakhjain23 <parakhjain2301@gmail.com>
2024-12-20 12:40:41 +0530

    ..

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,24 +1,41 @@
 
 import ChatBot from 'chatbot-react-native-sdk';
-import React from 'react';
-import { Button, DeviceEventEmitter, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { Button, DeviceEventEmitter, View } from 'react-native';
 
 function Home() {
-  const [threadId, setThreadId] = React.useState('12345');
+  const [threadId, setThreadId] = React.useState(null);
+  const [embedToken, setEmbedToken] = React.useState(null);
+  const [bridgeName, setBridgeName] = React.useState("Assistantt");
+
+  useEffect(() => {
+    setTimeout(() => {
+      setEmbedToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcmdfaWQiOiIxMTYyMSIsImNoYXRib3RfaWQiOiI2NzQ4MWJhMGM5NjFhNDQ5ZDczZDAxNmYiLCJ1c2VyX2lkIjoiMjMifQ.5-nzxW9GmHeItiCS445udYiaJzWGNIEZM2HpyVy-Cs4");
+      setThreadId("12345");
+    }, 2000);
+    setTimeout(() => {
+      setBridgeName('Assistant');
+    }, 10000);
+  }, [])
+
   const changeThreadId = () => {
+    // DeviceEventEmitter.emit('openChatbot', { type: 'openChatbot' });
+    // const randomThreadId = Math.random().toString(36).substring(2, 10);
+    // setThreadId(randomThreadId);
     DeviceEventEmitter.emit('openChatbot', { type: 'openChatbot' });
     // const randomThreadId = Math.random().toString(36).substring(2, 10);
     // setThreadId(randomThreadId);
   }
+
   return (
     <View style={{ flex: 1 }}>
-      {/* <Button title="Change Thread ID" onPress={changeThreadId} /> */}
-      <View style={{ width: 200, height: 600, justifyContent: 'center', alignItems: 'center', backgroundColor: 'yellow', position: 'static'}}>
-        <ChatBot threadId={threadId} bridgeName='demo' embedToken="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcmdfaWQiOiIxMjg5IiwiY2hhdGJvdF9pZCI6IjY2NTk2YWU3ZjA0NGRlNzMzZTNlYzdlYiIsInVzZXJfaWQiOiIxMjM0In0.WPAYYTuTFoXaLJLrA_SR14eym4lxjtiJpamyk2GBOTE" openInContainer={false} />
+      <Button title="Change Thread ID" onPress={changeThreadId} />
+      <View style={{ width: 200, height: 600, justifyContent: 'center', alignItems: 'center', position: 'static' }}>
       </View>
-
+      <ChatBot threadId={threadId} bridgeName={bridgeName} embedToken={embedToken} />
     </View>
   )
 }
 
 export default Home
+
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout() {
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
-      <StatusBar hidden />
+      {/* <StatusBar hidden /> */}
     </ThemeProvider>
   );
 }
